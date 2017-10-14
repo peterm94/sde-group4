3cf9ff099fd98df8cd3bdd769f8e3959634c62d6
Peter Mandile <peterm1994@hotmail.com>
2017-10-14 16:20:06 +1100

    Fixed typo in given php

--- v2/index.php
+++ v2/index.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-<form  action="welcome.php" method="post">
+<form  action="Welcome.php" method="post">
 FisrtName: <input type="text" name="firstname" value="<?php echo $fname;?>"><br/><br/>
 LastName:  <input type="text" name="lastname" value="<?php echo $lname;?>"><br/><br/>
 Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyre;?>"><br/><br/>
